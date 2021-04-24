3f1c9eb5603baacbdad5024b55da1cc4e2f81915
JaredWilliam <sutch002@gmail.com>
2021-04-23 20:57:31 -0500

    updated js and got the password to generate

--- script.js
+++ script.js
@@ -12,56 +12,40 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// // ------------My Code-----------//
+// // ------------------------------My Code----------------------------------//
 
 var special = " !\"/#$%&'()*+,-./:;<=>?@{|}~";
 var num = "0123456789";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = lower.toUpperCase();
 var Final = "";
+var passwordLength = 0;
+var password = "";
 
 // Prompt to select password
+function generatePassword() {
+  alert("Welcome to Jared's Password Generator! Good Luck!!!!");
 
-alert("Welcome to Jared's Password Generator! Good Luck!!!!");
-var passwordLength = prompt(
-  "Your password should be between 8 to 128 characters or its not gonna work."
-);
-var passLengthChecker = Number(passwordLength);
-var special = confirm(
-  "Would you like these special characters?  !\"/#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-);
-var Num = confirm("Would you like numeric characters 0-9?");
-var lower = confirm("Would you like lowercase characters?");
-var upper = confirm("Would you like uppercase characters?");
+  while (passwordLength > 128 || passwordLength < 8) {
+    passwordLength = prompt(
+      "Your password should be between 8 to 128 characters or its not gonna work."
+    );
+    // var passLengthChecker = Number(passwordLength);
+    if (passwordLength > 128 || passwordLength < 8) {
+      alert("WRRRRRROOOONNNNG!");
+    }
+  }
 
-// Button variables
-
-var generateBtn = document.querySelector("#gen");
-var copyBtn = document.querySelector("#copy");
-var passwordBox = document.querySelector("#pass-final");
-
-// function check to see if password is between 8-128
-
-// var choseLengthCorrectly = function () {
-//   alert("You must select a password length between 8 and 128");
-//   passwordLength = prompt(
-//     "How many characters would you like your password to be? Must be between 8 and 128."
-//   );
-//   passLengthChecker = Number(passwordLength);
-// };
-
-// function to check to make sure at least one type of character is selected
-
-var choseCharList = function () {
-  alert("You must select at least one of the 4 character types.");
-  special = confirm("Would you like special characters?");
-  Num = confirm("Would you like numeric characters?");
+  special = confirm(
+    "Would you like these special characters?  !\"/#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  );
+  num = confirm("Would you like numeric characters 0-9?");
   lower = confirm("Would you like lowercase characters?");
   upper = confirm("Would you like uppercase characters?");
-};
-// function to make final character list to select from for password generation and then returns the completed list
 
-var generateFinalCharList = function () {
+  return generatePass();
+}
+var generateFinal = function () {
   if (special === true) {
     Final += special;
   }
@@ -77,14 +61,15 @@ var generateFinalCharList = function () {
   return Final;
 };
 
-// Function to generate a random password and place password into password div
+// Function to generate a random password and place password
 
 var generatePass = function () {
-  // event.preventDefault()
-  var password = "";
-  var charList = generateFinalCharList();
+  password = "";
+
+  var charList = generateFinal();
   for (var i = 0; i < passwordLength; i++) {
-    pass += charList[Math.floor(Math.random() * charList.length)];
+    password += charList[Math.floor(Math.random() * charList.length)];
   }
-  passwordBox.textContent = pass;
+  return password;
 };
+// Button variables
